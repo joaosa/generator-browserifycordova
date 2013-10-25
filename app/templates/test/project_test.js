e82602dfd9eb9e15cba9637885c0f70ed4532bbb
Joao Andrade <jjadonotenter@gmail.com>
2013-10-25 16:26:14 +0100

    Add missing comma

--- app/templates/test/project_test.js
+++ app/templates/test/project_test.js
@@ -6,7 +6,7 @@ var <%= camelName %> = require('../lib/<%= projectName %>.js');
 
 describe('<%= props.name %>', function() {
   describe('#awesome()', function() {
-    it('should be awesome' function(){
+    it('should be awesome', function() {
       <%= camelName %>.awesome().should.equal('awesome');
     });
   });
