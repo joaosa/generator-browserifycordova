683bee72ac97e011160e49d345118b725524fefd
Joao Andrade <jjadonotenter@gmail.com>
2016-05-26 20:47:54 +0100

    Use yeoman-test

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var path    = require('path');
-var helpers = require('yeoman-generator').test;
+var helpers = require('yeoman-test');
 
 describe('browserifycordova generator', function () {
   beforeEach(function (done) {
